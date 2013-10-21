d19caa63d2774fac685c4a1c9cf01d656feec6ef
Chris Herwig <chris@mapbox.com>
2013-10-20 23:01:04 -0400

    demo-ify

--- s3.js
+++ s3.js
@@ -1,6 +1,7 @@
 var loc;
 var url;
 jQuery(function($) {
+
   if (typeof BUCKET_URL != 'undefined') {
     url = BUCKET_URL;
   } else {
@@ -8,10 +9,17 @@ jQuery(function($) {
     console.log('url : ' + location);
     loc = location;
   }
-  document.getElementById('h1').innerHTML = 'Index of ' + location.href;
-  $.get(url)
+  url = 'http://mapbox-blog-testing.s3.amazonaws.com'
+  // loc = ''
+
+  // document.getElementById('h1').innerHTML = 'Index of ' + location.href;
+  index = 'http://snap-index.s3.amazonaws.com'
+
+  $.get(index)
     .done(function(data) {
       var xml = $(data);
+      //console.log(xml)
+
       var files = $.map(xml.find('Contents'), function(item) {
         item = $(item);
         return {
@@ -26,25 +34,63 @@ jQuery(function($) {
       alert('There was an error');
       console.log(error);
     });
+
+
+
 });
+function preview(id){
+  var sizes = [75, 150, 240, 320, 500, 640, 800, 1024, 1280, 'native']
+  var crop = [128, 256, 512]
+
+  var list = document.getElementById('listing');
+  list.innerHTML = '';
+  var content = document.createElement('div');
+  content.className = 'col12 pad4 dark'
+  content.innerHTML = '<a class="thumb" href="#' 
+        + id 
+        + '"><img src="'  
+        +  url 
+        + '/640/' 
+        + id
+        + '.jpg" /></a>';
+  list.appendChild(content);
+
+  for (var i=0;i<sizes.length;i++){
+    var row = document.createElement('pre');
+    row.innerHTML = '<code>' 
+        +  url 
+        + '/' 
+        + sizes[i] 
+        + '/' 
+        + id 
+        + '.jpg</code>';
+    content.appendChild(row);
+  }
+
+}
 
 
 function renderTable(files) {
   var cols = [ 55, 45, 15 ];
-  var content = '';
+  var content = document.getElementById('listing');
+  content.innerHTML = ''
   var first_subdir = true;
   var snaps = $.grep(files, function(f) {
-    return f.Key.split('/')[0] === "native";
+    return f.Key.split('/')[0] === "photos";
    });
-  console.log(snaps)
   $.each(snaps, function(idx, item) {
     var key = item.Key.split('/')[1].split('.')[0]
     var row = '';
-    row += '<a href="' + url + '/index.html?path=' + 'native/' + key + '.png"><img src="'  +  url + '/crop/256/' + key + '.jpg" /><pre><code>' + key + '</pre></code></a>';
-    content += row;
- 
+    var thumb = document.createElement('a');
+    thumb.className = 'thumb col4 pad2';
+    thumb.setAttribute('href', "#" + key);
+    thumb.innerHTML = '<img src="'  +  url + '/crop/256/' + key + '.jpg" /><code>' + key + '</code>';
+    thumb.onclick = function(){
+         var uid = $(this)[0].hash.split('#')[1];
+         preview(uid);
+    };
+    content.appendChild(thumb);
   });
-  document.getElementById('listing').innerHTML = '<pre>' + content + '</pre>';
 }
 
 
--- snap.sh
+++ snap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-image="$1";
 bucket="mapbox-blog-testing";
+index='snap-index'
 
 function usage(){
   echo -e "usage:\n ./upload image.png"; 
@@ -14,16 +14,22 @@ function check(){
    if ! which s3cmd > /dev/null; then echo -e "Please run:\n pip install s3cmd"; fi
    if ! which convert > /dev/null; then echo -e "Please run:\n pip install imagemagick"; fi
    if ! which jpegtran > /dev/null; then echo -e "Please run:\n brew install jpeg-turbo"; fi
+
 }
 
+if ! check >/dev/null; then check; exit 1; fi
+
 function size_up(){
+
     #      generate a bunch of thumbnails and cropped subsets of input images
     img="$1";
     uid=`md5 "$img" | awk -F'=' '{print $2}' | tr -d ' '`
     mkdir -p $uid;
 
     ext=`echo "$img" | awk -F'.' '{ print $NF }'`
-    
+    echo "" > $uid/$uid;
+    s3cmd put -P --no-progress $uid/$uid s3://${index}/photos/ >/dev/null;
+
     s3cmd put -P --no-progress "$img" s3://${bucket}/native/$uid.${ext} >/dev/null;
 
     for res in 75 150 240 320 500 640 800 1024 1280; do
@@ -34,6 +40,8 @@ function size_up(){
     convert -gravity Center -crop 1024x1024+0+0 "$img" $uid/${uid}_1024.jpg >/dev/null;
     convert -resize 512x512 $uid/${uid}_1024.jpg $uid/${uid}_512.jpg >/dev/null;
     convert -resize 256x256 $uid/${uid}_1024.jpg $uid/${uid}_256.jpg >/dev/null;
+    convert -resize 128x128 $uid/${uid}_1024.jpg $uid/${uid}_128.jpg >/dev/null;
+
     for res in 128 256 512; do
             jpegtran -copy none -progressive $uid/${uid}_${res}.jpg > $uid/${uid}_${res}-1.jpg ;
             s3cmd put -P --no-progress $uid/${uid}_${res}-1.jpg s3://${bucket}/crop/$res/${uid}.jpg >/dev/null;
@@ -41,4 +49,20 @@ function size_up(){
     rm -rf $uid;
 }
 
+function delete(){
+    uid="$1"
+    for res in 128 256 512; do
+       s3cmd del s3://${bucket}/crop/$res/${uid}.jpg >/dev/null;
+    done;
+    for res in 75 150 240 320 500 640 800 1024 1280; do
+        s3cmd del s3://${bucket}/$res/${uid}.jpg >/dev/null;
+    done;
+    s3cmd del `s3cmd ls s3://$bucket/native/ | grep $uid | head -n 1 | awk '{print $4}'` >/dev/null;
+    s3cmd del s3://$index/photos/$uid >/dev/null
+}
 
+if [[ -z "$2" ]]; then
+    size_up "$1"
+elif [[ "$1" == 'del' ]]; then
+   delete "$2"
+fi
